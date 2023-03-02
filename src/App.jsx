9f4347f383e7563552fbf4e0fe4d8214b745e45a
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-03-02 11:57:54 +0300

    fetching advice

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [advice, setAdvice] = useState("");
 
-  return (
-    <div className="App">
-     
-    </div>
-  )
+  useEffect(() => {
+    getAdvice().then(result => {
+      // console.log(result);
+      setAdvice(result)
+    }) 
+  }, []);
+
+  async function getAdvice() {
+    let response = await fetch("https://api.adviceslip.com/advice");
+    if (response.ok) {
+      let json = await response.json();
+      return json.slip;
+    } else {
+      alert("Ошибка HTTP: " + response.status);
+    }
+  }
+
+  return <div className="App">{
+    advice ? advice.advice : 'asking for advice'
+  }</div>;
 }
 
-export default App
+export default App;
