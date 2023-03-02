dc7f3e66af2f55c19beb54186988caf6c31c2920
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-03-02 15:37:49 +0300

    footer style

--- src/components/styled/Footer.styled.js
+++ src/components/styled/Footer.styled.js
@@ -11,6 +11,16 @@ const StyledFooter = styled.footer`
   justify-content: center;
   align-items: center;
   transform: translate(0, 50%);
+  margin-top: 0.3em;
+  cursor: pointer;
+  transition: box-shadow 0.5s;
+  &:hover {
+    box-shadow: 0 0 27px 10px hsla(150, 100%, 66%, 0.4);
+  }
+
+  &:active {
+    transform: scale(0.94) translate(0, 50%);
+  }
 `;
 
 export default StyledFooter
